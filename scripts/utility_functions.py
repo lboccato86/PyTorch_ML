a72ea8c650e89e4a9163d4c451de0257001f0fd3
Levy <lboccato@unicamp.br>
2024-01-24 11:18:14 -0300

    Updates: now common/important functions are saved into scripts folder and notebooks import them

--- scripts/utility_functions.py
+++ scripts/utility_functions.py
@@ -33,50 +33,6 @@ def check_class_balance(label_tensor: torch.Tensor,
 
     return dict
 
-from typing import Dict, List
-import matplotlib.pyplot as plt
-
-def plot_loss_curves(results: Dict[str, List[float]]):
-    """Plots training curves of a results dictionary.
-
-    Args:
-        results (dict): dictionary containing list of values, e.g.
-            {"train_loss": [...],
-             "train_acc": [...],
-             "val_loss": [...],
-             "val_acc": [...]}
-    """
-    
-    # Get the loss values of the results dictionary (training and validation)
-    loss = results['train_loss']
-    val_loss = results['val_loss']
-
-    # Get the accuracy values of the results dictionary (training and test)
-    accuracy = results['train_acc']
-    val_accuracy = results['val_acc']
-
-    # Figure out how many epochs there were
-    epochs = range(len(results['train_loss']))
-
-    # Setup a plot 
-    plt.figure(figsize=(15, 7))
-
-    # Plot loss
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs, loss, label='train_loss')
-    plt.plot(epochs, val_loss, label='val_loss')
-    plt.title('Loss')
-    plt.xlabel('Epochs')
-    plt.legend()
-
-    # Plot accuracy
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs, accuracy, label='train_accuracy')
-    plt.plot(epochs, val_accuracy, label='val_accuracy')
-    plt.title('Accuracy')
-    plt.xlabel('Epochs')
-    plt.legend();
-
 import torch
 
 """ Função que computa os logits e os labels estimados dado um modelo treinado e um conjunto de amostras de entrada 
@@ -84,7 +40,7 @@ import torch
 def predict_model(model: torch.nn.Module,
                   X: torch.Tensor,
                   task: str,
-                  device=device,
+                  device=None,
                   ):
 
     # Put model in eval mode
@@ -111,7 +67,7 @@ def predict_model_dataloader(model: torch.nn.Module,
                              dataloader: torch.utils.data.DataLoader,
                              task: str,
                              n_classes: int,
-                             device=device,
+                             device=None,
                             ):
 
     # Put model in eval mode
